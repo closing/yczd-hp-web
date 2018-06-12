4e11a6c9342ef53dd56bf47a20c4115b560015b6
closing <zhu.yuxing@hotmail.com>
2018-06-12 17:26:45 +0900

    Update and rename src/main/java/com/yczd/hp/web/sample/formdata/UserForm.java to src/main/java/com/yczd/hp/web/common/formdata/UserForm.java

---
+++ src/main/java/com/yczd/hp/web/common/formdata/UserForm.java
@@ -0,0 +1,185 @@
+package com.yczd.hp.web.common.formdata;
+
+public class UserForm {
+	private String id;
+	private String userType;
+	private String userName;
+	private String mobilePhone;
+	private String email;
+	private String drivingLicense;
+
+	private String name;
+	private String sex;
+	private String birthday;
+
+	private String address1;
+	private String address2;
+	private String address3;
+	private String address4;
+
+	private String contact;
+	private String companyName;
+	private String telphone1;
+	private String telphone2;
+	private String telphone3;
+	private String telphone;
+
+	private String subscribe;
+
+	public String getDrivingLicense() {
+		return drivingLicense;
+	}
+
+	public void setDrivingLicense(String drivingLicense) {
+		this.drivingLicense = drivingLicense;
+	}
+
+	public String getTelphone1() {
+		return telphone1;
+	}
+
+	public void setTelphone1(String telphone1) {
+		this.telphone1 = telphone1;
+	}
+
+	public String getTelphone2() {
+		return telphone2;
+	}
+
+	public void setTelphone2(String telphone2) {
+		this.telphone2 = telphone2;
+	}
+
+	public String getTelphone3() {
+		return telphone3;
+	}
+
+	public void setTelphone3(String telphone3) {
+		this.telphone3 = telphone3;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getMobilePhone() {
+		return mobilePhone;
+	}
+
+	public void setMobilePhone(String mobilePhone) {
+		this.mobilePhone = mobilePhone;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getAddress3() {
+		return address3;
+	}
+
+	public void setAddress3(String address3) {
+		this.address3 = address3;
+	}
+
+	public String getAddress4() {
+		return address4;
+	}
+
+	public void setAddress4(String address4) {
+		this.address4 = address4;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getTelphone() {
+		return telphone;
+	}
+
+	public String getSubscribe() {
+		return subscribe;
+	}
+
+	public void setSubscribe(String subscribe) {
+		this.subscribe = subscribe;
+	}
+
+}
