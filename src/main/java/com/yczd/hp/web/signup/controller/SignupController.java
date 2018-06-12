87b01ddc57a2f78f6b8e3fbbcbeb9fddd358fc15
closing <zhu.yuxing@hotmail.com>
2018-06-12 17:29:34 +0900

    Update SignupController.java

--- src/main/java/com/yczd/hp/web/signup/controller/SignupController.java
+++ src/main/java/com/yczd/hp/web/signup/controller/SignupController.java
@@ -5,16 +5,22 @@ import java.util.List;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.thymeleaf.util.StringUtils;
 
 import com.yczd.hp.web.common.daodata.Area;
 import com.yczd.hp.web.common.formdata.AreaForm;
+import com.yczd.hp.web.common.formdata.UserForm;
 import com.yczd.hp.web.common.service.IAreaService;
+import com.yczd.hp.web.common.service.IUserService;
 
 @Controller
 @RequestMapping("/signup")
@@ -22,10 +28,15 @@ public class SignupController {
 
 	@Resource
 	IAreaService areaService;
+	@Resource
+	IUserService userService;
 
-	@RequestMapping(value = { "/", "" }, method = RequestMethod.GET)
-	public String signupForm(Model model, HttpServletRequest req) {
+	@RequestMapping(value = { "/{type}" }, method = RequestMethod.GET)
+	public String signup(Model model, HttpServletRequest req, @PathVariable("type") String type) {
+
 		model.addAttribute("contextPath", req.getContextPath());
+		model.addAttribute("user", new UserForm());
+		// DropdownList
 		List<Area> list = areaService.getListOfArea(0);
 		List<AreaForm> items = new ArrayList<AreaForm>();
 		AreaForm item;
@@ -35,13 +46,34 @@ public class SignupController {
 			items.add(item);
 		}
 		model.addAttribute("areaList", items);
-		return "/signup";
+
+		// person  :10
+		// supplyer:20
+		// company :21
+		// station :30
+		// courier :40
+
+		if (StringUtils.isEmpty(type) ||
+				(!"company".equals(type) && !"person".equals(type) && !"courier".equals(type)
+						&& !"station".equals(type) && !"supplyer".equals(type))) {
+
+			type = "person";
+		}
+		return "wechat/signup_" + type;
 
 	}
 
-	@RequestMapping(method = RequestMethod.POST)
-	public String signup(Model model) {
-		return "/signupsuccess";
+	@RequestMapping(value = "/{type}", method = RequestMethod.POST)
+	public String signup(@Valid UserForm form, Errors error, Model model, @PathVariable("type") String type) {
+		// Type
+		if (StringUtils.isEmpty(type) ||
+				(!"company".equals(type) && !"person".equals(type) && !"courier".equals(type)
+						&& !"station".equals(type) && !"supplyer".equals(type))) {
+
+			return "redirect:/signup/person";
+		}
+
+		return "wechat/signup_success";
 
 	}
 }
